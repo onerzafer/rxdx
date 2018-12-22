90359b9676d09dae945c78a40bd88828800e7030
Öner <onerizafer@gmail.com>
2018-12-22 20:53:29 +0100

    sample app removed

--- sample-app/app/utils/local-storage-manager.js
+++
@@ -1,8 +0,0 @@
-export function readLocal(key, defaultVal) {
-    const localString = localStorage.getItem(key);
-    return localString ? JSON.parse(localString) : defaultVal || undefined;
-}
-
-export function writeLocal(key, val) {
-    localStorage.setItem(key, JSON.stringify(val));
-}
