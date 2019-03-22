fa35b4146c64095d307a9e5099127e0ff2077896
Mictian <mictian@live.com>
2019-03-22 11:03:46 +0000

    added unit test infrastrucutre

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports ={
+  "presets": [
+    ["@babel/preset-env", {
+      "modules": false,
+      "targets": {
+        "browsers": "ie >= 11"
+      }
+    }]
+  ],
+  "env": {
+    "test": {
+      "presets": [["@babel/preset-env"]]
+    }
+  }
+};
