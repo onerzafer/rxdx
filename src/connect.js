69d1f997e9aa060f5796757e1ec2a6c7d2872a88
Mictian <mictian@live.com>
2019-03-27 15:21:59 +0000

    Added connect documentation

--- src/connect.js
+++ src/connect.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+/**
+ * Function that create a HoC that will connect the inner (passed-in) component the the rxdx store.
+ * Exeucting a re-render when subscriptions (selction from the store) specified by propsToSubscribe
+ * generate a state change in the HoC component.
+ *
+ * @param propsToSubscribe Object where each porperty is a RxJs subject responsible for selector
+ * the piece of state from the store that the current component is responsible for handling
+ *
+ * @param WrappedComponent React inner component to be connected to the store
+ */
 const connect = propsToSubscribe => WrappedComponent => {
   return class ConnectedComponent extends React.Component {
     constructor(...args) {
